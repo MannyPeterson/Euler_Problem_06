ab9037afb5a8945884b3e774d3ad36df1f3a0804
manny <manny@ego>
2019-05-18 17:02:44 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,12 @@
 
 public class Main {
 	public static void main(String[] args) {
-
+		long sumOfSquare = 0, squareOfSum = 0;
+		for (long i = 1; i < 101; i++) {
+			sumOfSquare += (i * i);
+			squareOfSum += i;
+		}
+		squareOfSum = squareOfSum * squareOfSum;
+		System.out.format("ANSWER = %d\n", squareOfSum - sumOfSquare);
 	}
 }
